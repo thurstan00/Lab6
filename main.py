99e9610197f082d224a1a410ebc0cde9c4c29a6b
trentford <fordy52.pf@gmail.com>
2023-10-27 16:26:52 -0400

    added decode method and option for decoding

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ def encode(password):
         encoded += str((int(password[i]) + 3) % 10)
     return encoded
 
+def decode(encodedPassword):
+    decodedPassword = ""
+    for num in encodedPassword:
+        if num >= 3 and num <= 9:
+            decodedPassword += str(int(num) - 3)
+        if num == 0:
+            decodedPassword += "7"
+        if num == 1:
+            decodedPassword += "8"
+        if num == 2:
+            decodedPassword += "9"
+
 def main():
     while True:
         # Print menu with encoder and decoder options
@@ -27,6 +39,10 @@ def main():
                 password = input("Enter your password: ")
             encoded_password = encode(password)
             print(encoded_password)
+
+        if option == 2:
+            decodedPassword = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decodedPassword}")
 
         if option == 3:
             break
